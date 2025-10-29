dbf5ed8129d114ab020e72513f31caeeb3878fe5
Balassa Hodi <hodi.balassa@gmail.com>
2025-10-29 14:56:46 +0100

    Read real data from json file implemented in read_json.py script

--- Python/main.py
+++ Python/main.py
@@ -4,8 +4,11 @@ The functions are determined in the other files in the folder.
 """
 
 import timeit
+
 from determine_cell_index import determine_cell_index
 from ray_tracing import ray_tracing
+from read_json import read_json
+
 from classes.robot_position import RobotPosition
 from classes.cell import Cell
 from classes.lidar_sensor_data import LidarData
@@ -35,32 +38,94 @@ def main(args=None):
     #############
 
     #############
-    # ray_tracing debug
-    startTime = timeit.default_timer() * 1000.0
-    freeCells, endCell = ray_tracing(
-        RobotPosition(X=7.78, Y=9.32, Theta=155.0),
-        LidarData(alpha=200.0, length=100.0),
-        n=15,
-        m=110,
-        w=1.0,
+    # # ray_tracing debug
+    # startTime = timeit.default_timer() * 1000.0
+    # freeCells, endCell = ray_tracing(
+    #     RobotPosition(X=7.78, Y=9.32, Theta=155.0),
+    #     LidarData(alpha=200.0, length=100.0),
+    #     n=15,
+    #     m=110,
+    #     w=1.0,
+    # )
+    # stopTime = timeit.default_timer() * 1000.0
+
+    # print("ray_tracing debug:")
+    # print("The free cells are:")
+    # for cell in freeCells:
+    #     print(f"({cell.row}, {cell.column})", end=" ")
+
+    # print()
+
+    # print(f"The last occupied cell: ({endCell.row}, {endCell.column})")
+
+    # print()
+
+    # print("The total number of free cells:", len(freeCells))
+    # print(f"The runtime of the ray tracing code: {stopTime - startTime} ms.")
+
+    # print()
+    #############
+
+    #############
+    # # read_json debug
+    # startTime = timeit.default_timer() * 1000.0
+    # lidarDataList = read_json()
+    # stopTime = timeit.default_timer() * 1000.0
+
+    # print("read_json debug:")
+    # print(
+    #     f"The total number of data items read from the json file: {len(lidarDataList)}"
+    # )
+    # print(f"The runtime of the read_json code: {stopTime - startTime} ms.")
+
+    # print()
+    #############
+
+    #############
+    # debug ray_tracing with real data from json
+    print("ray_tracing debug with real data:")
+
+    lidarDataList = read_json()
+    dataNumber = 0
+    robotPosition = lidarDataList[dataNumber]["robot_position"]
+
+    # the json file contains X-Y coordinates that are negative or too small (so probably the coordinate system is not set to the left-bottom corner)
+    # that's why the coordinates are modified here for testing purpose (in order not the get an error)
+    robotPosition.X = 5.0
+    robotPosition.Y = 5.0
+
+    print(
+        f"Robot position: X={robotPosition.X}, Y={robotPosition.Y}, Theta={robotPosition.Theta}"
     )
+
+    startTime = timeit.default_timer() * 1000.0
+
+    for i in range(len(lidarDataList[dataNumber]["scans"])):
+        lidarData = lidarDataList[dataNumber]["scans"][i]
+
+        # here the n, m, w values are set for testing purpose, they should be determined according to the grid map
+        freeCells, endCell = ray_tracing(robotPosition, lidarData, n=500, m=500, w=0.1)
+
+        # # this part is commented out to measure only the runtime of the ray tracing function
+        # print("The free cells are:")
+        # for cell in freeCells:
+        #     print(f"({cell.row}, {cell.column})", end=" ")
+
+        # print()
+
+        # print(f"The last occupied cell: ({endCell.row}, {endCell.column})")
+
+        # print()
+
+        # print("The total number of free cells:", len(freeCells))
+
+        # print()
+
     stopTime = timeit.default_timer() * 1000.0
 
-    print("ray_tracing debug:")
-    print("The free cells are:")
-    for cell in freeCells:
-        print(f"({cell.row}, {cell.column})", end=" ")
-
-    print()
-
-    print(f"The last occupied cell: ({endCell.row}, {endCell.column})")
-
-    print()
-
-    print("The total number of free cells:", len(freeCells))
-    print(f"The runtime of the ray tracing code: {stopTime - startTime} ms.")
-
-    print()
+    print(
+        f"The runtime of the ray tracing code for {len(lidarDataList[dataNumber]['scans'])} data: {stopTime - startTime} ms."
+    )
     #############
 
 
---
+++ Python/read_json.py
@@ -0,0 +1,91 @@
+"""
+This script reads the json file that contains the data from a lidar sensor.
+The json file is located in the folder 'data' with the name 'real_data.json'.
+The structure of the json file is the following:
+
+{
+    "data": [
+    {
+        "scan": [
+            <here are 360 float values representing distances in meters>
+            <the index of each value represents the angle in degrees from 0 to 359>
+            <this angle is relative to the orientation of the robot>
+            <the value 0.0 represents that the lidar sensor did not detect any obstacle in that direction>
+        ],
+        "t": <float timestamp in some dimension. Not important for data processing.>,
+        "pose": [
+            <the position and orientation of the robot when the scan was taken>
+            <x, y, theta are all floats, theta is in radians>
+        ]
+    },
+    ...
+    ]
+}
+
+The output:
+A list of dictionaries, each dictionary contains a RobotPosition key and a list of LidarData.
+The structure:
+
+Output = [
+    {
+        "robot_position": RobotPosition(X=json.data[i].pose[0], Y=json.data[i].pose[1], Theta=json.data[i].pose[2]),
+        "scans": [
+            LidarData(alpha=json.data[i].indexof(scan[j]), length=json.data[i].scan[j]),
+            LidarData(alpha=json.data[i].indexof(scan[j+1]), length=json.data[i].scan[j+1])
+            ...
+        ]
+    },
+    ...
+]
+
+"""
+
+import json
+from pathlib import Path
+import math
+
+from classes.robot_position import RobotPosition
+from classes.lidar_sensor_data import LidarData
+
+HERE = Path(__file__).resolve().parent
+DATA_PATH = HERE / "data" / "real_data.json"
+
+
+def read_json(filePath=DATA_PATH) -> list[dict[RobotPosition, list[LidarData]]]:
+    """
+    This function reads the specified json file and returns the data in a structured format. \n
+    ---
+    The output: \n
+    A list of dictionaries, where each dictionary contains a RobotPosition and a list of LidarData.
+    """
+    print(f"Reading the json file from: {filePath}")
+
+    with open(filePath, "r", encoding="utf-8") as jsonFile:
+        rawJsonData = json.load(jsonFile)
+
+    outputDataList = []
+
+    for item in rawJsonData["data"]:
+        # read the position and scan keys from an item of the data list
+        # one item is a dictionary that has the keys: "scan", "t", "pose"
+        # the "pose" contains a list of three floats: [X, Y, Theta]
+        # the "scan" contains a list of 360 float values representing distances in meters
+        position = item["pose"]
+        scan = item["scan"]
+
+        robotPosition = RobotPosition(
+            X=position[0], Y=position[1], Theta=math.degrees(position[2])
+        )
+
+        scans = []
+        for i in range(len(scan)):
+            # the scan that doesn't produce length is left behind, so that there are no unnecessary data in the output
+            if scan[i] == 0.0:
+                continue
+
+            lidarData = LidarData(alpha=i, length=scan[i])
+            scans.append(lidarData)
+
+        outputDataList.append({"robot_position": robotPosition, "scans": scans})
+
+    return outputDataList
